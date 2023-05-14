9cc4e1ece655e1683ac09c38fc4bcd44c53d1a80
Col-Haisell <66448199+Col-Haisell@users.noreply.github.com>
2023-05-14 14:57:33 +1000

    Add files via upload

---
+++ CybliminalPowerShellChallenge2.ps1
@@ -0,0 +1,10 @@
+$UpDownInput = Get-Content ".\UpDown-Input-File.txt"
+foreach($dest in $UpDownInput){
+    $upOrDown = Test-Connection -ComputerName $dest -Quiet -Count 1 -ErrorAction SilentlyContinue
+    if($upOrDown){
+         Write-Host($dest + " is up at " + (Get-Date))
+     }
+     else{
+        Write-Host($dest + " is down at " + (Get-Date))
+     }
+}
