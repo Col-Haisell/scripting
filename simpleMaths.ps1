92ecdbe0cc373d25e7d8176f656629ed238e6042
Col-Haisell <colan.haisell@gmail.com>
2023-06-05 14:10:31 +1000

    yes

--- simpleMaths.ps1
+++ simpleMaths.ps1
@@ -1,4 +1,10 @@
-write-host "To add two numbers together type the letter a. Two subtract one number from another type the letter s"
-$value1 = "10"
-$value2 = "20"
-[int]$value1 + [int]$value2
+write-host "To do some simple maths, type the first number, then Add or Subtract and finally the second number. You will see the result"
+$firstNumber = Read-Host "Please enter the first number"
+$addOrSubtract = Read-Host "Type add or subtract?"
+$secondNumber = Read-Host "Please enter the second number"
+#[int]$value1 + [int]$value2
+if($addOrSubtract -eq "add"){
+    Write-Host([int]$firstNumber + [int]$secondNumber)}
+else{
+    Write-Host([int]$firstNumber - [int]$secondNumber)
+}
